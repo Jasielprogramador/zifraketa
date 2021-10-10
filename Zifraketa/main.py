e512e79fd3734cb57a3b0367967603b1242bf820
Jasielprogramador <asierrosa@gmail.com>
2021-10-10 13:38:17 +0200

    falta acabar

--- Zifraketa/main.py
+++ Zifraketa/main.py
@@ -1,3 +1,4 @@
+import string
 from collections import Counter
 
 text = "RIJ AZKKZHC PIKCE XT ACKCUXJHX SZX," \
@@ -20,16 +21,18 @@ letters = Counter(text)
 
 alfabeto_mayus = []
 for i in range(65, 91):
+    if(i == 79):
+        alfabeto_mayus.append('Ñ')
     alfabeto_mayus.append(chr(i))
 
 
 freq_sp = {'e':16.78, 'a':11.96, 'o':8.69, 'l':8.37, 's':7.88, 'n':7.01, 'd':6.87, 'r':4.94, 'u':4.80, 'i':4.15,
            't':3.31, 'c':2.92, 'p':2.776, 'm':2.12, 'y':1.54, 'q':1.53, 'b':0.92, 'h':0.89, 'g':0.73, 'f':0.52,
-           'v':0.39, 'j':0.30, 'ñ':0.29, 'z':0.15, 'x':0.06, 'k':0.00}
+           'v':0.39, 'j':0.30, 'ñ':0.29, 'z':0.15, 'x':0.06, 'k':0.00, 'w':0.00}
 
 emaitza = Counter(alfabeto_mayus)
 
-texto_plano = text.replace(",","").replace(".","").replace(" ","").upper()
+texto_plano = text.replace(",","").replace(".","").replace(" ","").replace("[","").upper()
 
 for i in letters:
     if alfabeto_mayus.__contains__(i):
@@ -39,7 +42,7 @@ print(emaitza)
 
 deszifratuta = texto_plano.replace("X", "E").replace('E', 'A').replace('K', 'O').replace('I', 'L').replace('C', 'S').replace(
     'J', 'N').replace('T', 'D').replace('A', 'R').replace('R', 'U').replace('Z', 'I').replace('H', 'T').replace('N', 'C') \
-    .replace('P', 'P').replace('D', 'M').replace('O', 'Y').replace('Q', 'Q').replace('Ñ', 'H').replace('W','G').replace('Y','F') \
-    .replace('S', 'V').replace('G', 'J').replace('V', 'Ñ').replace('U', 'Z').replace('M', 'X').replace('F','K').replace('L','W')
+    .replace('P', 'P').replace('D', 'M').replace('O', 'Y').replace('Q', 'Q').replace("B","B").replace("Ñ","H").replace('W', 'G').replace("Y","F").replace("S","V")\
+    .replace("G","J").replace("V","Ñ").replace("U","Z").replace("M","X").replace("F","K").replace("L","W")
 
 print("TEXTO DESCIFRADO: "+deszifratuta)
